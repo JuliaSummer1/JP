2a943adb32012b1a2d92d6bb3b6c326c5fc4f241
Julia <yula.julia.summer@gmail.com>
2021-09-12 00:27:11 +0300

    First HW Python

---
+++ Python_HW1.py
@@ -0,0 +1,41 @@
+String_1 = "Julia"
+Integer_1 = 26
+Float_1 = 1.6
+Bytes_1 = b'1,2,3,4,5'
+List_1 = ['red', True, 123, 'yellow']
+Tuple_1 = (True, 'lyceum', 1.25, 1)
+Set_1 = set([123, 124, 125, 125, 123])
+Frozenset_1 = frozenset({"Julia", "26"})
+Dict_1 = {'Name' : 'Julia', 'Surname' : 'Piatigorskaya'}
+
+
+print(String_1)
+print(Integer_1)
+print(Float_1)
+print(Bytes_1)
+print(List_1)
+print(Tuple_1)
+print(Set_1)
+print(Frozenset_1)
+print(Dict_1['Name'])
+
+
+print('Julia' + ' ' + 'Piatigorskaya')
+
+a = 23
+b = 51
+
+c = a + b
+print(c)
+
+d = a / b
+print(d)
+
+e = a * b
+print(e)
+
+g = a // b
+print(g)
+
+j = a % b
+print(j)
