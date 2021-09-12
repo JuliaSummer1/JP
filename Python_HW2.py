4fb2c30ce5e871d2e9871cc314ed19e9e78832ed
Julia <yula.julia.summer@gmail.com>
2021-09-12 23:41:48 +0300

    Second HW Python

---
+++ Python_HW2.py
@@ -0,0 +1,86 @@
+(1)
+Name = 'Julia'
+Surname = 'Piatigorskaya'
+
+(2)
+
+A = 3.542
+B = 535.563
+C = 3.5432
+
+(3)
+
+a = 27
+b = 56
+c = 178
+d = 8
+
+print()
+print(4)
+
+print(a > b)
+print (d < c)
+print (d != a)
+print (c <= d)
+print (d >= b)
+print (d > c)
+print (c <= a)
+print (a != b)
+print (d < a)
+print (d > c)
+print (b <= c)
+print (c < d)
+print (d < b)
+print (c != a)
+print (d == c)
+
+print()
+print(5)
+
+print(A == C)
+print(A > C)
+print(B <= C)
+print(A >= B)
+print(A < C)
+print(B != C)
+print(A <= C)
+print(A != C)
+print(B >= C)
+
+print()
+print(6)
+
+print (d > c or c >= a)
+print (c <= a and d != a)
+print (a != b or b > a)
+print (d < a and c == b)
+print (d > c or d < c)
+print (b <= c and b <= a)
+print (c < d or c != d)
+print (not d < b)
+print (not c != a)
+print (not d == c)
+
+print()
+print(7)
+
+Name = int(input("Вы ввели число = "))
+print(Name > 30)
+# or Name < 30 or Name == 30)
+
+print()
+print(8)
+
+import random
+Name = int(input("Вы ввели число = "))
+print(random.randint(1, 100), Name > random.randint(1, 100))
+
+
+print()
+print(9)
+
+import random
+Name = int(input("Вы ввели число = "))
+print(random.randint(1, 100), Name < random.randint(1, 100))
+print(random.randint(1, 100), Name < random.randint(1, 100))
+
